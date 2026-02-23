daab6417b7224b422671ab3e43f31f5d2c9b1cfd
matias <matiasidiartviera@gmail.com>
2026-02-23 11:56:20 +0100

    Arquitectura de Grandes Ligas: Configuración de Astro limpia

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,4 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-<<<<<<< HEAD
 export default defineConfig({});
-=======
-export default defineConfig({
-    site: 'https://agencialquimia.com'
-});
->>>>>>> d61dcb1f646af93037322112411d1cd919ca4299
